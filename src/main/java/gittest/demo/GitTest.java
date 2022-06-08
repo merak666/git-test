47d05092fb9b04189d3212c5eac621aab4b8b798
Lisanna <1321347340@qq.com>
2022-06-08 23:16:33 +0800

    first commit

---
+++ src/main/java/gittest/demo/GitTest.java
@@ -0,0 +1,11 @@
+package gittest.demo;
+
+/**
+ * @author 雪霁初晴
+ * @date 2022/6/8 23:11
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
